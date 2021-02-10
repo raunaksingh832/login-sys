aa585f98376dc6f4dd5c0cad7b5c6262c1b087b1
Raunak Singh <60030141+raunaksingh832@users.noreply.github.com>
2021-02-10 16:21:49 +0530

    Update Main.py
    FINAL COMMIT !!

--- Main.py
+++ Main.py
@@ -10,14 +10,19 @@ engine = sqlalchemy.create_engine('mysql+pymysql://root:raunakcodes2625@localhos
 main_window = Tk()
 login_window = Tk()
 sign_up_window = Tk()
+main_window.title('MAIN WINDOW')
+login_window.title('LOGIN')
+sign_up_window.title('SIGN_UP')
 username = Entry(login_window)
 password = Entry(login_window)
 New_Username = Entry(sign_up_window)
 New_password = Entry(sign_up_window)
 New_Email = Entry(sign_up_window)
 OTP = Entry(sign_up_window)
-passwords = pd.read_sql_table('accounts',engine,columns=['password_'])
-usernames = pd.read_sql_table('accounts',engine,columns=['username'])
+username_query = 'SELECT username FROM accounts'
+password_query = 'SELECT password_ FROM accounts'
+usernames = str(pd.read_sql_query(username_query,engine)).split()
+passwords = str(pd.read_sql_query(password_query,engine)).split()
 def window():
     global username    
     global password
@@ -30,88 +35,66 @@ def window():
     def Login():
         global good_pass
         global good_username
-        Label(login_window,text='Username').pack()
+        Label(login_window,text='Username',fg='red').pack()
         username.pack()
-        Label(login_window,text='Password').pack()
+        Label(login_window,text='Password',fg='blue').pack()
         password.pack()
         def login():
             global good_pass
             global good_username
             for user in usernames:
                 if username.get() not in usernames:
-                    notification.Notify(
-                        title = 'FAIL',
-                        message = 'BAD USERNAME',
-                        app_icon=None,
-                        toast = False
-                    )
+                    Label(login_window,text='*NO such username',bg='red').pack()
                     good_username = False
                 elif username.get() in usernames:
                     good_username = True
             for pas in passwords:
                 if password.get() not in passwords:
-                    notification.Notify(
-                        title = 'FAIL',
-                        message = 'BAD PASSWORD',
-                        app_icon=None,
-                        toast = False
-                        )
+                    Label(login_window,text='*incorrect password',bg='red').pack()
                     good_pass = False 
                 elif password.get() in passwords:
                     good_pass = True 
             if good_pass == True and good_username == True :
-                notification.Notify(
-                        title = 'Success',
-                        message = 'successfully logged in :)',
-                        app_icon=None,
-                        toast = False
-                    )
+                Label(login_window,text='SIGN IN SUCCESSFULLY',bg='blue').pack()
+        Button(login_window,text='LOGIN',command=login,bg='blue').pack()
         login_window.mainloop()
-        Button(login_window,text='LOGIN',command=login).pack()
     def sign_up():
         Otps = list(range(5000,10000))
         shuffle(Otps)
         x = Otps[6]
-        Label(sign_up_window,text='Username').pack()
+        Label(sign_up_window,text='Username',fg='blue').pack()
         New_Username.pack()
-        Label(sign_up_window,text='Password').pack()
+        Label(sign_up_window,text='Password',fg='red').pack()
         New_password.pack()
-        Label(sign_up_window,text='Email').pack()
+        Label(sign_up_window,text='Email',fg='blue').pack()
         New_Email.pack()
-        Label(sign_up_window,text='OTP').pack()
+        Label(sign_up_window,text='OTP',fg='red').pack()
         OTP.pack()
         def send_otp():
             smtp_object = smtplib.SMTP('smtp.gmail.com',587)
             smtp_object.ehlo()
             smtp_object.starttls()
-            email = 'raunakprogrammer@gmail.com'
-            password = 'ohskexslaukiwaff'
+            email = 'raunak.priya.dinkar@gmail.com'
+            password = 'mbpzcoqjrigbuher'
             smtp_object.login(email,password)
             to = New_Email.get()
             subject = 'Your login OTP'
             content = 'your OTP is' + str(x)
             msg =  subject + '\n' + content 
             smtp_object.sendmail(email,to,msg)
-        if OTP.get() == x:
-            Query = f'''insert into accounts values({New_Username.get()},{int(New_password.get())},{New_Email.get()})'''
-            pd.read_sql_query(Query,engine)
-            notification.Notify(
-                title = 'SUCCESS',
-                message= 'signed up successfully',
-                app_icon = None,
-                toast=False
-            )
-        else:
-            notification.Notify(
-                title='Fail',
-                message = 'Failed Signing Up',
-                app_icon = None,
-                toast = False 
-            )
+        def sign():
+            if OTP.get() == str(x):
+                Query = f'''insert into accounts values('{New_Username.get()}',{int(New_password.get())},'{New_Email.get()}')'''
+                with engine.begin() as conn:
+                    conn.execute(Query)
+                Label(sign_up_window,text='SIGNED up !',bg='blue').pack()
+            else:
+                Label(sign_up_window,text='BAD OTP',bg='red').pack()
+        Button(sign_up_window,text='Send otp',command=send_otp,bg='green').pack()
+        Button(sign_up_window,text='SIGN UP', command=sign,bg='purple').pack()
         sign_up_window.mainloop()
-        Button(sign_up_window,text='Send otp',command=send_otp).pack()
-    Button(main_window,text='Login',command=Login).pack()
-    Button(main_window,text='Sign up',command=sign_up).pack()
+    Button(main_window,text='Login',command=Login,bg='blue').pack()
+    Button(main_window,text='Sign up',command=sign_up,bg='red').pack()
     main_window.mainloop()
 if __name__ == '__main__':
     window()
